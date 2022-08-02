14dc607f1d621130b70c7d49fa01642a7f997770
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-08-02 16:27:00 +0200

    async await

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,10 @@
-function getPictureOfTheDay() {
+/*================================================================*\
+*                                                                 *
+*                          FETCH promise                          *
+*                                                                 *
+\*================================================================*/
+
+/* function getPictureOfTheDay() {
     fetch('https://api.nasa.gov/planetary/apod?api_key=obcpDMJFMpsYBaFm04u6tY6Zn0ctSap6iouRct1G')
     .then((response) => response.json())
     .then((data) => {
@@ -17,4 +23,34 @@ function pictureComponent(pictureData) {
             <p>${pictureData.explanation}</p>
             </div>
     `
+} */
+
+/*================================================================*\
+*                                                                 *
+*                          FETCH async await                      *
+*                                                                 *
+\*================================================================*/
+
+async function getPictureOfTheDay() {
+  const response = await fetch(
+    "https://api.nasa.gov/planetary/apod?api_key=obcpDMJFMpsYBaFm04u6tY6Zn0ctSap6iouRct1G"
+  );
+  const data = await response.json();
+
+  const root = document.querySelector("#root");
+  const picture = pictureComponent(data);
+  root.innerHTML = picture;
+  console.log("2");
+  console.log("1");
+};
+
+await getPictureOfTheDay();
+
+function pictureComponent(pictureData) {
+  return `
+        <div>
+            <img src="${pictureData.url}" alt="pic of the day"/>
+            <p>${pictureData.explanation}</p>
+            </div>
+    `;
 }
