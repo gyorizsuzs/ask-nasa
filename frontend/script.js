877152cb4c1b1d3613ca2a84521df82ad5ec5e30
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-09-04 19:45:25 +0200

    past dates

--- frontend/script.js
+++ frontend/script.js
@@ -31,7 +31,71 @@ function pictureComponent(pictureData) {
 *                                                                 *
 \*================================================================*/
 
-async function getPictureOfTheDay() {
+const key = "obcpDMJFMpsYBaFm04u6tY6Zn0ctSap6iouRct1G";
+const url = "https://api.nasa.gov/planetary/apod?api_key=";
+
+let datePickerComponent = function () {
+  return `
+    <input type="date" id="date">
+  `;
+};
+
+//take json from the api and format it with online formatter so we can see the parameters like date, media_type, url of the picture etc »»
+
+function mediaComponent(mediaData) {
+  if (mediaData.media_type === "image") {
+    return `
+      <h1>Picture of the Day<h1>
+      <p>${mediaData.date}</p>
+      <img src="${mediaData.url}"/>
+      <p>${mediaData.explanation}</p>
+    `; // here we manage the cases when we receive an image with the data, then we have to manage it when we receive something else, e.g. a video
+  } else {
+    return `
+    <h1>Picture of the Day<h1>
+    <p>${mediaData.date}</p>
+    <iframe width="560" height="315" src="${mediaData.url}" freeborder="0" allor="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <p>${mediaData.explanation}</p>
+    `;
+  }
+}
+
+function pastContentCreator() {
+  const xDate = "&date=" + document.getElementById("date").value;
+
+  fetch(url + key + xDate)
+    .then((response) => response.json())
+    .then((data) => {
+      const content = document.querySelector("#content");
+      const picture = mediaComponent(data);
+      content.innerHTML = picture;
+    });
+}
+
+function loadEvent() {
+  console.log("Page loaded");
+
+  let datePickerElement = document.querySelector("#datepicker");
+  datePickerElement.insertAdjacentHTML("beforeend", datePickerComponent());
+  const datePicker = document.querySelector("#date");
+  datePicker.addEventListener("change", pastContentCreator);
+
+  function getPicture() {
+    fetch(url + key)
+      .then((response) => response.json())
+      .then((data) => {
+        const content = document.querySelector("#content");
+        const picture = mediaComponent(data);
+        content.innerHTML = picture;
+      });
+  }
+
+  getPicture();
+}
+
+window.addEventListener("load", loadEvent);
+
+/* async function getPictureOfTheDay() {
   const response = await fetch(
     "https://api.nasa.gov/planetary/apod?api_key=obcpDMJFMpsYBaFm04u6tY6Zn0ctSap6iouRct1G"
   );
@@ -42,7 +106,7 @@ async function getPictureOfTheDay() {
   root.innerHTML = picture;
   console.log("2");
   console.log("1");
-};
+}
 
 await getPictureOfTheDay();
 
@@ -53,4 +117,4 @@ function pictureComponent(pictureData) {
             <p>${pictureData.explanation}</p>
             </div>
     `;
-}
+} */
